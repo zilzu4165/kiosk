1ef13a5878b04fe9a64d6417698488da3e1f3dff
js.hwang <js.hwang@tosspayments.com>
2021-07-18 03:32:05 +0900

    통합 Test 하는 법

--- src/main/java/me/zilzu/kiosk/order/OrderService.java
+++ src/main/java/me/zilzu/kiosk/order/OrderService.java
@@ -14,23 +14,11 @@ public class OrderService {
     }
 
     public void doOrder(OrderSaveRequest orderSaveRequest) {
-        // Controller, Service --> OrderSaveRequest
-        // Repository --> Order
         Order order = orderSaveRequest.toOrder();
         orderMapper.doOrder(order);
     }
 
-
-    // 2021-07-17T10:00:00
     public Order getOrder(String orderId) {
-        Order order = orderMapper.getOrder(orderId);
-
-        // 오늘 주문한 건 이면, 에러 로그를 남긴다.
-//        LocalDate orderDate = order.getOrderTime().toLocalDate();
-//        if (orderDate.isEqual(LocalDate.now())) {
-//            log.error("아 오늘 주문한 건인데 왜 조회하니?");
-//        }
-
-        return order;
+        return orderMapper.getOrder(orderId);
     }
 }
---
+++ src/test/java/me/zilzu/kiosk/order/OrderServiceTest.java
@@ -0,0 +1,40 @@
+package me.zilzu.kiosk.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@Sql(value = {"classpath:basic_dml.sql"})
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    public void doOrder_기본_테스트() {
+        // Order table에 넣는 과정
+        OrderSaveRequest orderSaveRequest = new OrderSaveRequest();
+        orderSaveRequest.setOrderId(1L);
+        orderSaveRequest.setTotalPrice(100);
+        orderSaveRequest.setIsTakeOut(true);
+        orderSaveRequest.setPaymentMethod("CARD");
+        orderSaveRequest.setUserId(1L);
+        orderSaveRequest.setStoreId(1L);
+        orderService.doOrder(orderSaveRequest);
+
+        // Order table에서 꺼내오는 과정
+        Order order = orderService.getOrder("1");
+
+        assertThat(order.getOrderId()).isEqualTo(1L);
+        assertThat(order.getTotalPrice()).isEqualTo(100);
+        assertThat(order.getTakeOut()).isEqualTo('Y');
+        assertThat(order.getPaymentMethod()).isEqualTo("CARD");
+        assertThat(order.getUserId()).isEqualTo(1L);
+        assertThat(order.getStoreId()).isEqualTo(1L);
+    }
+}
