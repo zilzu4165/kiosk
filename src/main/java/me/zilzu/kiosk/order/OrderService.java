1919519bbc47ccbebf16f920aa01170b5a618005
zilzu4165 <zilzu4165@naver.com>
2021-07-19 13:58:07 +0900

    kiosk-20 order 생성 및 조회 API 구현 및 테스트 구현 완료

--- src/main/java/me/zilzu/kiosk/order/OrderService.java
+++ src/main/java/me/zilzu/kiosk/order/OrderService.java
@@ -1,8 +1,12 @@
 package me.zilzu.kiosk.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
+@Slf4j
 public class OrderService {
 
     private final OrderMapper orderMapper;
@@ -12,10 +16,21 @@ public class OrderService {
     }
 
     public void doOrder(OrderSaveRequest orderSaveRequest) {
-        orderMapper.doOrder(orderSaveRequest);
+        // Controller, Service --> OrderSaveRequest
+        // Repository --> Order
+        Order order = orderSaveRequest.toOrder();
+        orderMapper.doOrder(order);
     }
+
 
     public Order getOrder(String orderId) {
         return orderMapper.getOrder(orderId);
+
+        // 오늘 주문한 건 이면, 에러 로그를 남긴다.
+//        LocalDate orderDate = order.getOrderTime().toLocalDate();
+//        if (orderDate.isEqual(LocalDate.now())) {
+//            log.error("오늘 주문한 건인데 왜 조회해?");
+//        }
+
     }
 }
---
+++ src/test/resources/basic_dml.sql
@@ -0,0 +1,8 @@
+insert into `user`(finger_print)
+values ('12312839128');
+
+insert into `owner`(`name`, email, phone_number, business_number, `id`, `password`)
+values ('박성수', 'pfpf@gmail.com', '1023010310', '2138213', 'zilzu', 'idjsioadj');
+
+insert into `store`(store_name, menu, location, tel, open_time, close_time, owner_id)
+values ('엄지칼국수', '칼국수', '수영구', '010-3333-3333', '17:00', '18:00', '1');
