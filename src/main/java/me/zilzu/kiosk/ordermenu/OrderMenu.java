7adb75f5f91b33433bbbeb35eb34e2fb3eea9053
zilzu4165 <zilzu4165@naver.com>
2021-07-26 16:21:37 +0900

    수량과 가격에 대한 비즈니스 로직 필요

--- src/main/java/me/zilzu/kiosk/ordermenu/OrderMenu.java
+++ src/main/java/me/zilzu/kiosk/ordermenu/OrderMenu.java
@@ -4,8 +4,11 @@ import lombok.Data;
 
 @Data
 public class OrderMenu {
-    private int quantity;
-    private int price;
+    // 수량과 가격에 대해
+    // 비즈니스 로직 필요
+
+    private Integer quantity;
+    private Integer price;
     private Long orderId;
     private Long menuId;
 
